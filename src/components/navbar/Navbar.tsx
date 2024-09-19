5e6c6a77666a90d653f77424d1468f9e74abbf3f
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2024-09-18 23:56:33 -0600

    Refactor favicon and metadata in index.html

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,7 +5,9 @@ import { MenuColapse } from "./MenuColapse"
 function Navbar() {
   return (
     <nav className="flex items-center justify-between p-4">
-        <Link to="/" className="text-3xl font-bold">App</Link>
+        <Link to="/" className="text-3xl font-bold">
+        <img src="/spring.svg" alt="logo de spring boot" className="w-10 h-10" />
+        </Link>
         <div className="hidden sm:flex items-center gap-3">
             <Link to="/" className="px-3 py-1.5 hover:bg-muted rounded-md">Home</Link>
             <Link to="/about" className="px-3 py-1.5 hover:bg-muted rounded-md">About</Link>
